a8e3ea4796fa9f3124c3136b26f6d344a29901a1
Ken@LredsOffice <ken@lreds.com>
2014-03-11 16:46:35 +0800

    Added Table on Select Zodiac VC

--- zodiac/ZodiacSelectViewController.m
+++ zodiac/ZodiacSelectViewController.m
@@ -8,7 +8,9 @@
 
 #import "ZodiacSelectViewController.h"
 
-@interface ZodiacSelectViewController ()
+@interface ZodiacSelectViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong)NSArray *aryZodiac;
 
 @end
 
@@ -26,11 +28,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // for Debug purpose
-    UIView *tmpView = [[UIView alloc] initWithFrame:(CGRect){{50.0, 50.0}, {100.0, 100.0}}];
-    tmpView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:tmpView];
+    
+    _aryZodiac = @[@"白羊座", @"金牛座", @"双子座", @"巨蟹座", @"狮子座", @"处女座", @"天秤座", @"天蝎座", @"射手座", @"摩羯座", @"水瓶座", @"双鱼座"];
+    
+    [self setupZodiacTable];
 }
 
 - (void)didReceiveMemoryWarning
@@ -38,5 +39,44 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)setupZodiacTable {
+    CGRect bounds = self.view.bounds;
+    CGRect tblFrame = CGRectMake(0, 0.0f, CGRectGetWidth(bounds), CGRectGetHeight(bounds));
+    NSLog(@"Betfeeds VC Table frame:  %@",NSStringFromCGRect(tblFrame));
+    
+    UITableView* tblZodiac = [[UITableView alloc] initWithFrame:tblFrame style:UITableViewStylePlain];
+    tblZodiac.delegate = self;
+    tblZodiac.dataSource = self;
+    tblZodiac.separatorStyle = UITableViewCellSeparatorStyleNone;
+    //    self.tblBetfeeds.backgroundColor = [UIColor whiteColor];
+    tblZodiac.backgroundColor = [UIColor redColor];
+    
+    [self.view addSubview:tblZodiac];
+}
+
+#pragma mark -
+#pragma mark UITableView DataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _aryZodiac.count;
+}
+
+#pragma mark UITableView Delegate
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = (NSString*)_aryZodiac[indexPath.row];
+    return cell;
+}
+
 
 @end
